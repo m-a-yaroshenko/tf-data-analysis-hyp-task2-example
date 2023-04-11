93eb2029241189c78915dea791e18f040f3c88b1
m-a-yaroshenko <114859682+m-a-yaroshenko@users.noreply.github.com>
2023-04-11 09:39:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,8 +7,10 @@ import scipy.stats as stats
 chat_id = 257112106 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    stat, p_value = ks_2samp(x, y)
+    stat, pvalue = ks_2samp(x, y)
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return True if p_value <= 0.01 else False # Ваш ответ, True или False
+    alpha = 0.01
+    crit = np.sqrt(-0.5 * np.log(alpha/2) / len(x))
+    return True if stat > crit else False # Ваш ответ, True или False
