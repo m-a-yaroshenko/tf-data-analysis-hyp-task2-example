522fdab5a91e3c014e8fb8381a7692f48e907e89
m-a-yaroshenko <114859682+m-a-yaroshenko@users.noreply.github.com>
2023-04-13 11:12:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,14 @@
-from scipy.stats import ks_2samp
 import pandas as pd
 import numpy as np
-import scipy.stats as stats
+from scipy.stats import anderson_ksamp
 
 
 chat_id = 257112106 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    stat, pvalue = ks_2samp(x, y)
+    statistic, critical_values, pvalue = anderson_ksamp([x, y])
+    alpha = 0.01
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 0.01
-    crit = np.sqrt(-0.5 * np.log(alpha/2) / len(x))
-    return True if stat > crit else False # Ваш ответ, True или False
+    return True if statistic > critical_values[2] else False # Ваш ответ, True или False
